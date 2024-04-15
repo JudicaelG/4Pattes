ffe3159114ded812ee8c4d79bd5f53588dd70d0c
judicael <judicaelguillaume@outlook.fr>
2024-04-15 11:32:45 +0200

    Ajout des informations de chaque animal de l'utilisateur dans le carrousel de la page d'accueil

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,14 +2,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Animals;
 
 
 class IndexController extends AbstractController{
     #[Route('/', name: 'index')]
-    public function index():Response{
+    public function index(EntityManagerInterface $entityManager):Response{
+
+
+        $animalsOfTheUser = $entityManager->getRepository(Animals::class)
+        ->getConnectedUserAnimals($this->getUser()->GetId());
+
         return $this->render('index.html.twig',[
+            'animalsOfTheUser' => $animalsOfTheUser
         ]);
     }    
 }
